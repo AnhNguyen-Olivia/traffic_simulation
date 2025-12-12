3276fe12e6b96442f3d237d290c8c2c8a2576be4
AnhNguyen-Olivia <thuyanhnguyen.vietnam@gmail.com>
2025-12-12 16:33:53 +0100

    Update gui demo and pull from trainbao

--- src/main/java/real_time_traffic_simulation_with_java/gui/MainWindow.java
+++ src/main/java/real_time_traffic_simulation_with_java/gui/MainWindow.java
@@ -4,25 +4,27 @@ import real_time_traffic_simulation_with_java.alias.Path;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 
 public class MainWindow extends Stage {
 
+    //Sepreate the main window into 3 part using BorderPane
+
     public MainWindow(){
         initializeGui();
     }
 
     private void initializeGui(){
-        Group root = new Group();
-        Scene scene = new Scene(root, Color.BLACK);
+        //Sepreate the main window into 3 part using BorderPane
+        BorderPane root = new BorderPane();
+        Scene scene = new Scene(root,1400, 830, Color.WHITE);
 
         Image icon = new Image(getClass().getResource(Path.IconImage).toExternalForm());
         this.getIcons().add(icon);
         this.setTitle("Traffic Simulation Beta");
-        this.setWidth(1400);
-        this.setHeight(850);
         this.setResizable(false);
         this.setScene(scene);
         this.show();
