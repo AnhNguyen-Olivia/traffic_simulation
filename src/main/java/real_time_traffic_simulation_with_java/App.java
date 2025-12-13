75a3ce669b2f357a18224dfc5e4bf768ffd74089
AnhNguyen-Olivia <thuyanhnguyen.vietnam@gmail.com>
2025-12-13 23:09:19 +0100

    Update simulation

--- src/main/java/real_time_traffic_simulation_with_java/App.java
+++ src/main/java/real_time_traffic_simulation_with_java/App.java
@@ -1,10 +1,13 @@
 package real_time_traffic_simulation_with_java;
 import javafx.application.Platform;
+import real_time_traffic_simulation_with_java.cores.SimulationEngine;
 import real_time_traffic_simulation_with_java.gui.MainWindow;
 
 //import real_time_traffic_simulation_with_java.gui.MainWindow;
 
 public class App {
+    private SimulationEngine simulationEngine;
+    private volatile boolean isRun = true;
 
     public static void main(String[] args) {
         App app = new App();
@@ -22,18 +25,35 @@ public class App {
     private void launchGui(){
         Platform.startup(() -> {
             try {
+                simulationEngine = new SimulationEngine();
                 MainWindow mainWindow = new MainWindow();
                 mainWindow.show();
+
+                startSimThread();
             } catch (Exception e) {
                 e.printStackTrace();
             }
         });
     }
 
-    // private void runSimulation() throws Exception{
-    //     while(true){
-    //         connection.nextStep();
-    //         Thread.sleep(100);
-    //     }
-    // }
+    private void startSimThread(){        
+        Thread simulationThread = new Thread(() -> {
+            try {
+                while (isRun){
+                    simulationEngine.stepSimulation();
+                    Thread.sleep(50);
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        });
+        simulationThread.setDaemon(true);
+        simulationThread.start();
+    }
+
+    public void shutdown(){
+        isRun = false;
+    }
+    
 }
+
