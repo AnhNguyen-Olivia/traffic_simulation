c5171b926c6e77a77029ab104e3f28fcade2e5a1
Tran Bao <10423111@student.vgu.edu.vn>
2025-12-15 02:35:41 +0700

    Update inline comment

--- src/main/java/real_time_traffic_simulation_with_java/gui/mapLayer/trafficlightLayer.java
+++ src/main/java/real_time_traffic_simulation_with_java/gui/mapLayer/trafficlightLayer.java
@@ -6,7 +6,6 @@ import real_time_traffic_simulation_with_java.alias.Metrics;
 
 import java.util.List;
 
-import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.Tooltip;
 import javafx.scene.control.Label;
 import javafx.animation.Timeline;
@@ -61,10 +60,11 @@ public class trafficlightLayer extends Group {
     private void addToolTip(List<TrafficLightData> Tls) throws Exception {
         for (TrafficLightData Tl : Tls){
             Label tooltipLabel = new Label();
-            // Install tooltip
             Tooltip tooltip = new Tooltip();
+            // Tooltip does not repaint when visible if setText, Label is a live node that can be updated dynamically
+            // Tooltip text is treated as static String, tooltip graphic is treated as Node that can be updated dynamically
             tooltip.setGraphic(tooltipLabel);
-            tooltip.setContentDisplay(ContentDisplay.GRAPHIC_ONLY);
+            // Add Timeline to update tooltip content with simulation speed
             Timeline updateTooltip = new Timeline(new KeyFrame(Duration.ZERO, e -> {
                 try{
                     tooltipLabel.setText(simulationEngine.getTlTooltip(Tl.getId()));
@@ -72,10 +72,13 @@ public class trafficlightLayer extends Group {
                     ex.printStackTrace();
                 }
             }), new KeyFrame(Duration.millis(Metrics.CONNECT_SPEED_MS)));
+            // Ensure the timeline runs indefinitely
             updateTooltip.setCycleCount(Animation.INDEFINITE);
+            // To prevent tooltip delay and automatical hide
             tooltip.setShowDelay(Duration.ZERO);
             tooltip.setShowDuration(Duration.INDEFINITE);
             tooltip.setHideDelay(Duration.millis(Metrics.HIDE_DELAY));
+            // Update tooltip content when shown
             tooltip.setOnShown(e->updateTooltip.play());
             tooltip.setOnHidden(e->updateTooltip.stop());
             Tooltip.install(Tl, tooltip);
